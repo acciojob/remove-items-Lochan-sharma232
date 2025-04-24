03a9df45a0b351d6630d03b774f8ba56ca2c0486
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-24 17:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+let color=document.getElementById("colorSelect");
+let  btn1=document.querySelector("input");
+btn1.addEventListener("click",()=>{
+	if(color.selectedIndex!=-1){
+		color.remove(color.selectedIndex)
+	}
+})
+	
